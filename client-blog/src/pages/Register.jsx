8966bb9dc60dfa9e178644b4a2b798ede531ecf3
chayan-DUET <chayan_cse10@yahoo.com>
2025-01-23 17:00:24 +0600

    create post and login

--- client-blog/src/pages/Register.jsx
+++ client-blog/src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {Link} from "react-router-dom"
 
 const Register = () => {
   const [userData, setUserData] = useState({
@@ -15,12 +16,17 @@ const Register = () => {
   }
   return (
     <section className="register">
-      <div className="containe">
+      <div className="container">
         <h2>Sign Up</h2>
         <form className="form register__form">
           <p className="form__error-message">This is an error message</p>
-          <input type="text" placeholder='Full Name' value={userData.name} onChange={changeInputHandler} />
+          <input type="text" placeholder='Full Name' name='name' value={userData.name} onChange={changeInputHandler} autoFocus />
+          <input type="text" placeholder='Email' name='email' value={userData.email} onChange={changeInputHandler} />
+          <input type="password" placeholder='Password' name='password' value={userData.password} onChange={changeInputHandler} />
+          <input type="password" placeholder='confirm password' name='password2' value={userData.password2} onChange={changeInputHandler} />
+          <button type='submit' className='btn primary'>Register</button>
         </form>
+        <small> Already have an account? <Link to="/login"> Sign in</Link> </small>
       </div>
     </section>
   )
