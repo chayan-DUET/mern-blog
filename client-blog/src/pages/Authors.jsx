e7b3732b25e6f1722c4f72fc9b912d2905eae0ca
chayan-DUET <chayan_cse10@yahoo.com>
2025-01-25 16:59:06 +0600

    create profile

--- client-blog/src/pages/Authors.jsx
+++ client-blog/src/pages/Authors.jsx
@@ -8,11 +8,11 @@ import Avatar5 from '../images/avatar5.jpg'
 import { Link } from 'react-router-dom'
 
 const authorsData = [
-  {id: 1, avatar: Avatar1, name: 'Ernest', post: 3},
-  {id: 2, avatar: Avatar2, name: 'Chayan', post: 2},
-  {id: 3, avatar: Avatar3, name: 'Jon Doe', post: 5},
-  {id: 4, avatar: Avatar4, name: 'Dramani Mahama', post: 0},
-  {id: 5, avatar: Avatar5, name: 'Hajia Bintu', post: 1},
+  {id: 1, avatar: Avatar1, name: 'Ernest', posts: 3},
+  {id: 2, avatar: Avatar2, name: 'Chayan', posts: 2},
+  {id: 3, avatar: Avatar3, name: 'Jon Doe', posts: 5},
+  {id: 4, avatar: Avatar4, name: 'Dramani Mahama', posts: 0},
+  {id: 5, avatar: Avatar5, name: 'Hajia Bintu', posts: 1},
 ]
 
 const Authors = () => {
@@ -22,14 +22,18 @@ const Authors = () => {
       { authors.length >0 ? <div className="container authors__container">
         {
           authors.map(({id, avatar, name, posts}) => {
-            return <Link  key={id} to= {`/posts/users/${id}`}>
+            return <Link  key={id} to= {`/posts/users/${id}`} className='author'>
             <div className="author__avatar">
               <img src= {avatar} alt={`Image of ${name}`} />
+            </div>
+            <div className="author__info">
+              <h4> {name} </h4>
+              <p>{posts}</p>
             </div>
             </Link>
           })
         }
-      </div> : <h2> no users/authors found. </h2>}
+      </div> : <h2 className='center'> no users/authors found. </h2>}
     </section>
   )
 }
--- client-blog/src/pages/UserProfile.jsx
+++ client-blog/src/pages/UserProfile.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Avatar from '../images/avatar1.jpg'
+import { FaEdit } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 
 const UserProfile = () => {
+  const [avatar, setAvatar] = useState('')
   return (
-    <div>
-      
-    </div>
+    <section className="profile">
+      <div className="container profile__container">
+        <Link to={`/myposts/asaa`} className='btn'> My posts</Link>
+
+        <div className="profile__details">
+          <div className="avatar__wrapper">
+            <div className="profile_avatar">
+              <img src={Avatar} alt="" />
+            </div>
+            { /* Form to update avatar */}
+            <form  className="avatar__form">
+              <input type="file" name='avatar' id='avatar' onChange={ e => setAvatar(e.target.files[0])} accept='png, jpg, jpeg' />
+              <label htmlFor="avatar"> <FaEdit /> </label>
+
+            </form>
+            <button className='profile__avatar-btn'> <FaCheck /> </button>
+          </div>
+          <h1> Chayan </h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
